7ea95a3eff580de701fdb30b60d89c7648c9ae2b
notwinston <winstonthov@gmail.com>
2016-04-24 17:02:26 -0700

    Create test.go

---
+++ test.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Create variable of type String
+	// and assign the variable with a value
+	var myName string = "Jack"
+
+	var animal string = "dogs"
+
+	fmt.Println(myName + " is smart ")
+
+	fmt.Println(animal + " are cute ")
+
+}
